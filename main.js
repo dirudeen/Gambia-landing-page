b08862639c2eba10c15f6bee5d944f3c47e19fd7
Hadiru Colley <dirudeen22@gmail.com>
2023-07-19 20:05:33 +0300

    Renamed to Banjul

---
+++ main.js
@@ -0,0 +1,82 @@
+const data = [
+    {
+        img: "./images/senegambia.jpg",
+        country: 'Senegambia - The Gambia',
+        place: "Senegambia",
+        discribe:
+        `orem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Rerum illo, commodi quibusdam non maxime itaque perferendis
+        porro ad doloremque maiores dolorem quidem culpa dolores.
+        At optio deleniti hic ex ducimus.`
+
+    },
+    
+    {
+        img: "./images/soma.jpg",
+        country: 'Soma - The Gambia',
+        place: "Soma",
+        discribe:
+        `orem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Rerum illo, commodi quibusdam non maxime itaque perferendis
+        porro ad doloremque maiores dolorem quidem culpa dolores.
+        At optio deleniti hic ex ducimus.`
+
+    },
+    
+    {
+        img: "./images/serekunda.jpg",
+        country: 'Serekunda - The Gambia',
+        place: "Serekunda",
+        discribe:
+        `orem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Rerum illo, commodi quibusdam non maxime itaque perferendis
+        porro ad doloremque maiores dolorem quidem culpa dolores.
+        At optio deleniti hic ex ducimus.`
+        
+    },
+
+    {
+        img: "./images/banjul.jpg",
+        country: 'Banjul - The Gambia',
+        place: "Banjul",
+        discribe:
+        `orem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Rerum illo, commodi quibusdam non maxime itaque perferendis
+        porro ad doloremque maiores dolorem quidem culpa dolores.
+        At optio deleniti hic ex ducimus.`
+        
+    },
+    
+]
+
+
+const introduce = document.querySelector(".introduce");
+const ordinalNumber = document.querySelector('.ordinal-number');
+
+introduce.innerHTML = "";
+ordinalNumber.innerHTML = "";
+
+for (let i = 0; i < data.length; i++) {
+    introduce.innerHTML += `
+    <div class="wrapper">
+    <span>
+      <h5 class="country" style="--idx: 0">${data[i].country}</h5>
+    </span>
+    <span>
+      <h1 class="place" style="--idx: 1">${data[i].place}</h1>
+    </span>
+    <span>
+      <p class="describe" style="--idx: 2">${data[i].discribe}</p>
+    </span>
+    <span>
+      <button class="discover-button" style="--idx: 3">Discover Now</button>
+    </span>
+  </div>
+  `
+
+  ordinalNumber.innerHTML += `<h2>0${i + 1}</h2>`
+}
+
+introduce.children[0].classList.add("active")
+ordinalNumber.children[0].classList.add("active")
+
