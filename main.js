39943af2dac784fcebaa405873b6a221e7482171
Hadiru Colley <dirudeen22@gmail.com>
2023-07-20 23:08:22 +0300

    updated files

--- main.js
+++ main.js
@@ -87,12 +87,48 @@ const thumbnailListWraapper = document.querySelector(`.thumbnail-list .wrapper`)
 thumbnailListWraapper.innerHTML += `
  <div class="thumbnail zoom">
     <img src="${data[0].img}" alt="">
- </div>`;
+ </div>`; 
 
-for (let i = 1; i < data.length; i += 1) {
+for (let i = 1; i < data.length; i++) {
     thumbnailListWraapper.innerHTML += `
  <div class="thumbnail zoom" style="--idx: ${i - 1}">
     <img src="${data[i].img}" alt="">
  </div>`;
     
 }
+
+const nextBtn = document.querySelector('.navigation .next-button')
+let currentIndex = 0;
+nextBtn.addEventListener('click', () => {
+  nextBtn.disabled = true;
+  let clone = thumbnailListWraapper.children[0].cloneNode(true);
+  clone.classList.remove("zoom");
+  thumbnailListWraapper.appendChild(clone);
+  thumbnailListWraapper.children[1].classList.add("zoom");
+
+  setTimeout(()=> {
+    thumbnailListWraapper.children[0].remove()
+    nextBtn.disabled = false
+  },1000 )
+
+  for (let i = 2; i < thumbnailListWraapper.childElementCount; i++){
+    thumbnailListWraapper.children[i].style = `--idx: ${i - 2}`;
+
+  }
+  if (currentIndex < data.length - 1) {
+    currentIndex++
+  } else {
+    currentIndex = 0
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    introduce.children[i].classList.remove("active");
+    ordinalNumber.children[i].classList.remove('active');
+  }
+
+  introduce.children[currentIndex].classList.add('active');
+  ordinalNumber.children[currentIndex].classList.add('active');
+  ordinalNumber.children[currentIndex].textContent = `${currentIndex + 1}`;
+
+})
+
